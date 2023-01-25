bcb2225db65ee3b9ea8937c87cc61329be96d2fd
cibs84 <d.tilesi@gmail.com>
2023-01-25 15:55:45 +0100

    password generator completato

---
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -0,0 +1,33 @@
+package org.lessons.java.security;
+
+import java.util.Scanner;
+
+public class PasswordGenerator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String fname;
+		String lname;
+		String favColor;
+		byte birthDay;
+		byte birthMonth;
+		int birthYear;
+		String pwd;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Inserisci il tuo nome:");
+		fname = input.nextLine();
+		System.out.println("Inserisci il tuo cognome:");
+		lname = input.nextLine();
+		System.out.println("Inserisci il tuo colore preferito:");
+		favColor = input.nextLine();
+		System.out.println("Inserisci il giorno in cui sei nato:");
+		birthDay = input.nextByte();
+		System.out.println("Inserisci il mese in cui sei nato:");
+		birthMonth = input.nextByte();
+		System.out.println("Inserisci l'anno in cui sei nato:");
+		birthYear = input.nextInt();
+		pwd = fname + "-" + lname + "-" + favColor + "-" + (birthDay+birthMonth+birthYear);
+		System.out.println("Questa è la tua nuova password:" + pwd);
+	}
+}
