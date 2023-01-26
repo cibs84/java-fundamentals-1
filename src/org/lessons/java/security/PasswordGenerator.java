1d2d72bffcbf0ce12da5a514419fa65d35e2596f
cibs84 <d.tilesi@gmail.com>
2023-01-26 09:14:51 +0100

    refactor

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -28,6 +28,15 @@ public class PasswordGenerator {
 		System.out.println("Inserisci il giorno in cui sei nato:");
 		birthDay = input.nextInt();
 		
+		System.out.println("Inserisci il mese in cui sei nato:");
+		birthMonth = input.nextInt();
 		
+		System.out.println("Inserisci l'anno in cui sei nato:");
+		birthYear = input.nextInt();
+		
+		pwd = fname + "-" + lname + "-" + favColor + "-" + (birthDay+birthMonth+birthYear);
+		System.out.println("Questa è la tua nuova password:" + pwd);
+		
+		input.close();
 	}
 }
